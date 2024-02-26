e921fa9926256d868c3e5e1c61f3e373fb327b05
ElviraVargas7 <elviravarber@gmail.com>
2024-02-26 14:57:54 -0600

    login endpoint added with jwt

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,19 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    "corsheaders",
     'users'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -138,3 +138,10 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+APPEND_SLASH = False
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
--- api/users/urls.py
+++ api/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RegisterView, LoginView, UserView, LogoutView
 
 urlpatterns = [
-    path('create/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('/create', RegisterView.as_view()),
+    path('/login', LoginView.as_view()),
+    path('/user', UserView.as_view()),
+    path('/logout', LogoutView.as_view()),
 ]
